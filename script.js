82a1842818b12c0e19a1c8d10848830d6d85b881
LoudShield <sokozemx@gmail.com>
2026-01-03 02:13:12 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// Tema claro / oscuro
-const toggle = document.getElementById("themeToggle");
-const body = document.body;
-
-if (localStorage.getItem("theme") === "light") {
-    body.classList.add("light");
-    toggle.textContent = "🌙";
-}
-
-toggle.addEventListener("click", () => {
-    body.classList.toggle("light");
-    const isLight = body.classList.contains("light");
-    toggle.textContent = isLight ? "🌙" : "☀️";
-    localStorage.setItem("theme", isLight ? "light" : "dark");
-});
-
 // Animaciones al hacer scroll
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
